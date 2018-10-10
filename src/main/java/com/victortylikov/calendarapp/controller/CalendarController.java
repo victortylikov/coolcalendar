ea2a1664ea39db188e183c8f21539421ea3a04d1
victortylikov <ViktorTyulikov@coherentsolutions.com>
2018-10-10 19:04:46 +0300

    small_update

--- src/main/java/com/victortylikov/calendarapp/controller/CalendarController.java
+++ src/main/java/com/victortylikov/calendarapp/controller/CalendarController.java
@@ -2,6 +2,8 @@ package com.victortylikov.calendarapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Map;
@@ -14,10 +16,23 @@ public class CalendarController {
 
     @RequestMapping("/")
     public String index(Map<String, Object> model){
+
+
+
         LocalDate currentDate = LocalDate.now();
-        Month month = currentDate.getMonth();
-        Integer monthLength = month.length(currentDate.isLeapYear());
-        model.put("monthLength", monthLength);
+        LocalDate prevMonth = currentDate.minusMonths(1);
+        Integer currentMonthLength = currentDate.getMonth().length(currentDate.isLeapYear());
+        Integer prevMonthLength = prevMonth.getMonth().length(currentDate.isLeapYear());
+
+       // DayOfWeek nameDayForLastDayOfPrevMonth = prevMonth.withDayOfMonth(prevMonthLength);
+        LocalDate firstDateOfMonth = currentDate.withDayOfMonth(1);
+        DayOfWeek dayForFirstDay = firstDateOfMonth.getDayOfWeek();
+
+
+        System.out.println("First Date Of Month " + firstDateOfMonth.toString());
+        System.out.println("Name of first month day " + dayForFirstDay.getValue());
+        model.put("monthLength", currentMonthLength);
+        model.put("dayForFirstDay", dayForFirstDay.getValue());
         return "index";
     }
 
--- src/main/resources/static/javascript/dayScript.js
+++ src/main/resources/static/javascript/dayScript.js
@@ -1,4 +1,15 @@
-function myFunction(days) {
+function displayPrevMonth(days, lastDay) {
+    var container = document.getElementById("tableBody")
+    for (i = 1; i <= days; i++) {
+        var blockDiv = document.createElement("div");
+        blockDiv.className = "day";
+        blockDiv.append(i);
+        container.appendChild(blockDiv);
+    }
+}
+
+
+function displayCurrentMonth(days) {
     var container = document.getElementById("tableBody")
     for (i = 1; i <= days; i++) {
         var blockDiv = document.createElement("div");
