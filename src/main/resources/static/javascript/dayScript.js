44bd14a85999f99ff606cc2d544ff9ca390747ad
victortylikov <ViktorTyulikov@coherentsolutions.com>
2018-10-15 18:49:55 +0300

    small refactor

--- src/main/resources/static/javascript/dayScript.js
+++ src/main/resources/static/javascript/dayScript.js
@@ -1,15 +1,12 @@
-document.getElementById("body").addEventListener("load", displayCurrentMonth(21));
-
 function displayPrevMonth(days, lastDay) {
-    var container = document.getElementById("tableBody")
+    var container1 = document.getElementById("tableBody")
     for (i = 1; i <= days; i++) {
-        var blockDiv = document.createElement("div");
-        blockDiv.className = "day";
-        blockDiv.append(i);
-        container.appendChild(blockDiv);
+        var blockDiv1 = document.createElement("div");
+        blockDiv1.className = "day";
+        blockDiv1.append(i);
+        container1.appendChild(blockDiv);
     }
 }
-
 
 function displayCurrentMonth(days) {
     var container = document.getElementById("tableBody")
