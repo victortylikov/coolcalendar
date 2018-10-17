b4ab1c3f5288d88150f04eeeb88ba4d0cf12f7db
victortylikov <ViktorTyulikov@coherentsolutions.com>
2018-10-17 18:40:49 +0300

    add last month js

--- src/main/java/com/victortylikov/calendarapp/controller/CalendarController.java
+++ src/main/java/com/victortylikov/calendarapp/controller/CalendarController.java
@@ -16,8 +16,8 @@ public class CalendarController {
 
     @RequestMapping("/")
     public String index(Map<String, Object> model){
-        LocalDate currentDate = LocalDate.now();
-        LocalDate prevMonth = currentDate.minusMonths(1);
+        LocalDate currentDate = LocalDate.now().plusMonths(1);
+        LocalDate prevMonth = LocalDate.now().minusMonths(1).plusMonths(1);
         Integer currentMonthLength = currentDate.getMonth().length(currentDate.isLeapYear());
         Integer prevMonthLength = prevMonth.getMonth().length(currentDate.isLeapYear());
         LocalDate lastDateOfPrevMonth = prevMonth.withDayOfMonth(prevMonth.getMonth().length(prevMonth.isLeapYear()));
--- src/main/resources/static/javascript/dayScript.js
+++ src/main/resources/static/javascript/dayScript.js
@@ -1,10 +1,12 @@
 function displayPrevMonth(days, lastDay) {
-    var container1 = document.getElementById("tableBody")
-    for (i = 1; i <= days; i++) {
-        var blockDiv1 = document.createElement("div");
-        blockDiv1.className = "day";
-        blockDiv1.append(i);
-        container1.appendChild(blockDiv);
+    var container = document.getElementById("tableBody")
+    if(lastDay < 7){
+        for (i = days + 1 - lastDay; i <= days; i++) {
+            var blockDiv = document.createElement("div");
+            blockDiv.className = "day";
+            blockDiv.append(i);
+            container.appendChild(blockDiv);
+        }
     }
 }
 
