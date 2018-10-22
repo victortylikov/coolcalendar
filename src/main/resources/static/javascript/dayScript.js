85fa439a55b1ac99ee0071a5439abb617c17ef3a
victortylikov <ViktorTyulikov@coherentsolutions.com>
2018-10-22 18:49:45 +0300

    add grid layout

--- src/main/resources/static/javascript/dayScript.js
+++ src/main/resources/static/javascript/dayScript.js
@@ -1,8 +1,9 @@
 function displayPrevMonth(days, lastDay) {
-    var container = document.getElementById("tableBody")
+    var container = document.getElementById("grid-container")
     if(lastDay < 7){
         for (i = days + 1 - lastDay; i <= days; i++) {
             var blockDiv = document.createElement("div");
+            blockDiv.className = "item day";
             blockDiv.className = "day";
             blockDiv.append(i);
             container.appendChild(blockDiv);
@@ -11,10 +12,10 @@ function displayPrevMonth(days, lastDay) {
 }
 
 function displayCurrentMonth(days) {
-    var container = document.getElementById("tableBody")
+    var container = document.getElementById("grid-container")
     for (i = 1; i <= days; i++) {
         var blockDiv = document.createElement("div");
-        blockDiv.className = "day";
+        blockDiv.className = "item day";
         blockDiv.append(i);
         container.appendChild(blockDiv);
     }
