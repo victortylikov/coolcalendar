1112e103329a9aefce21c56d6f496bc2eef31108
victortylikov <ViktorTyulikov@coherentsolutions.com>
2018-11-15 20:49:57 +0300

    add event dialog

--- src/main/resources/static/javascript/createEvent.js
+++ src/main/resources/static/javascript/createEvent.js
@@ -1,8 +1,23 @@
-var btn1 = document.getElementsByClassName("month-name");
 var modal = document.getElementById('event-modal-id');
-var btn = document.getElementById("myBtn");
-alert("AAAAAAAA"+ btn);
-btn.onclick = function() {
-    alert("AAAAAAAA");
+modalCloseButton = document.getElementById('modal-close-button');
+
+window.addEventListener('load', function(){
+    var days = document.getElementsByClassName("day");
+    for (var i=0; i < days.length; i++) {
+      days[i].addEventListener('click', openEventDialog, false);
+    }
+});
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+var openEventDialog = function() {
     modal.style.display = "block";
+};
+
+modalCloseButton.onclick = function() {
+    modal.style.display = "none";
 }
